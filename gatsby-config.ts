799ed61349ae28d54170984f57f5452fc1887dc2
Lyuboslav Alexandrov <alexandrovcorp@yahoo.com>
2024-07-05 14:03:02 +0300

    Icons update

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -38,14 +38,14 @@ const config: GatsbyConfig = {
         display: `standalone`,
         icons: [
           {
-            src: `/android-chrome-192x192.png`,
-            sizes: `192x192`,
-            type: `image/png`,
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
           },
           {
-            src: `/android-chrome-512x512.png`,
-            sizes: `512x512`,
-            type: `image/png`,
+            src: "/android-chrome-384x384.png",
+            sizes: "384x384",
+            type: "image/png",
           },
         ],
       },
